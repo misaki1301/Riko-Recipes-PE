1738ab122d92de12ff12e47309cee2b782dd5a85
misaki1301 <paul.frankpc@gmail.com>
2023-01-07 21:50:13 -0500

    fixed service for supabase_service.dart

--- lib/services/supabase_service.dart
+++ lib/services/supabase_service.dart
@@ -4,9 +4,16 @@ final supabase = Supabase.instance.client;
 
 class SupabaseService {
   Future<List<Map<String, dynamic>>> getRecipes() async {
-    final future =
-        await supabase.from("recipes").select<List<Map<String, dynamic>>>();
-    print("future $future");
-    return future;
+    try {
+      final future =
+          await supabase.from("recipes").select<List<Map<String, dynamic>>>();
+      //final data = await supabase.from("recipes").select().single();
+      //print("data $data");
+      print("future $future");
+      return future;
+    } catch (e) {
+      print(e);
+      throw Exception(e);
+    }
   }
 }
